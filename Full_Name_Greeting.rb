42f76ddd15fdab50660bab655b4910cf08f6561f
Sinclair <sbk64@cornell.edu>
2014-05-24 23:40:30 -0400

    Chapter 5

---
+++ Full_Name_Greeting.rb
@@ -0,0 +1,7 @@
+puts 'What\'s your first name?'
+f_name = gets.chomp
+puts 'Excellent! ' + 'What\'s your middle name?'
+m_name = gets.chomp
+puts 'Thank you! ' + 'What\'s your last name?'
+l_name = gets.chomp
+puts 'Your full name is ' + f_name + ' ' + m_name + ' ' + l_name + '.'
